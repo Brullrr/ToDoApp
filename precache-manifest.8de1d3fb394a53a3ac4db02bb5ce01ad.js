e9aa25ae4dca219e70005d31406ec200083ba348
Brullrr <rbrull@yahoo.com>
2020-12-23 22:00:10 +0900

    Updates

---
+++ precache-manifest.8de1d3fb394a53a3ac4db02bb5ce01ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede2d013aca8404800ca5a950923ad8c",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "0cb4b82e854f16c7ff1f",
+    "url": "/ToDoApp/static/css/main.ec09e314.chunk.css"
+  },
+  {
+    "revision": "35dcc7f9e30ac176f3c4",
+    "url": "/ToDoApp/static/js/2.48f99102.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoApp/static/js/2.48f99102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb4b82e854f16c7ff1f",
+    "url": "/ToDoApp/static/js/main.39d73eb0.chunk.js"
+  },
+  {
+    "revision": "1022d9b6270d75fe8824",
+    "url": "/ToDoApp/static/js/runtime-main.7aabe7a2.js"
+  },
+  {
+    "revision": "089b5cdc96be8057308c084c387a6d8b",
+    "url": "/ToDoApp/static/media/HomeIcon.089b5cdc.png"
+  },
+  {
+    "revision": "889652d79ff6a0139aa1461846c4617b",
+    "url": "/ToDoApp/static/media/TransDrag.889652d7.png"
+  },
+  {
+    "revision": "6d2520b9e88494079af070241f953dc3",
+    "url": "/ToDoApp/static/media/concrete-wall-background.6d2520b9.jpg"
+  }
+]);
